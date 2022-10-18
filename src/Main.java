dab15057341382ed15b9758859d6b5f99bb432dc
montgomeri15 <montgomeri15@gmail.com>
2022-10-18 15:59:38 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private static enum DayOTheWeek{
+        MON, TUE, WED, THU, FRI, SAT, SUN
     }
+
+    public static void main(String[] args) {
+        var dayOfTheWeek= DayOTheWeek.MON;
+
+        switch (dayOfTheWeek){
+            case MON:
+                System.out.println("Понеділок");
+                break;
+            case TUE:
+                System.out.println("Вівторок");
+                break;
+            default:
+                System.out.println("Інший день тижня");
+        }
+    }
+
+
 }
