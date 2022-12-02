3bd1776d1d0efc76cf4c21f86f94cb1853cb16bc
montgomeri15 <montgomeri15@gmail.com>
2022-12-02 11:17:52 +0200

    Switch in Java 12 and 13 commit

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,7 @@ public class Main {
         String result = switch (dayOfTheWeek){
             case MON -> "Понеділок";
             case TUE, WED, THU, FRI -> {
-                System.out.println("Будній день, окрім понеділка");
-                yield "\t-- yield замінив у Java 13 break і повертає значення із блоку.";
+                yield "Будній день, окрім понеділка";
             }
             default -> "Вихідний день";
         };
